1dc1d90037dea324429efef0ed7d0b364b8a760a
Diksha206 <dikshabhalke10@gmail.com>
2025-07-15 16:22:02 +0530

    Add files via upload

---
+++ Pro.java
@@ -0,0 +1,26 @@
+package project;
+
+class Lon
+{
+	  int a=2;
+	  int b=3;
+	   void calcu()
+
+	  {
+		  System.out.println("The Addition is:"+a+b);
+		  System.out.println("The Multiplication is :"+a*b);
+		  System.out.println("The Division is:"+a/b);
+		  System.out.println("The Subtraction is:"+(a-b));
+		
+	  }
+	  
+}
+
+public class Pro {
+	public static void main(String[] args)
+	{
+	Lon c1=new Lon();
+	c1.calcu();
+	}
+
+}
