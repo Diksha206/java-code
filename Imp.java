89e9eabfc77b3beec554c7d2376790839b39f7b0
Diksha206 <dikshabhalke10@gmail.com>
2025-07-15 14:44:10 +0530

    Add files via upload

---
+++ Imp.java
@@ -0,0 +1,24 @@
+package project;
+class mode
+{
+	int Divident=17;
+	int Divisor=5;
+	void opr()
+	{
+	int remainder=Divident % Divisor;
+	System.out.println("the remainder is:"+remainder);
+		
+	}
+			
+}
+public class Imp {
+	public static void main(String[] args)
+	{
+	mode m1=new mode();
+	m1.opr();
+	
+
+	
+	}
+
+}
