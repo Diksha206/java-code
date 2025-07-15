dde7e1dc81028582b540ba563dd7c4192129a1b1
Diksha206 <dikshabhalke10@gmail.com>
2025-07-15 15:10:40 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,27 @@
+package project;
+class sq
+{
+     int Square;
+     int Cube;
+	 static void risk(int a)
+	 {
+		 int square=a*a;
+		 int cube=a*a*a;
+		 System.out.println("square is:"+square);
+		 System.out.println("cube is:"+cube);
+		 
+	 }
+}
+
+public class Square {
+	public static void main(String[] args)
+	
+	{
+		
+		sq.risk(2);
+		
+		
+	
+	}
+
+}
