af9dbb38064e34b35334af7467bb11d335502732
Diksha206 <dikshabhalke10@gmail.com>
2025-07-16 14:59:28 +0530

    Add files via upload

---
+++ Fahrenheitsc.java
@@ -0,0 +1,17 @@
+package importance;
+import java.util.Scanner;
+public class Fahrenheitsc {
+	public static void main(String[] arg)
+	{
+	Scanner sc=  new Scanner(System.in);
+	System.out.println("Enter the tempreture in celsius:");
+	short celsius=sc.nextShort();
+	System.out.println("tempreture in Celsius:" + celsius);
+	
+	System.out.println("tempreture in fahrenheit Fahrenheit:" + (celsius * (9/5) + 32) );
+	sc.close();
+	
+	
+	}	
+
+}
