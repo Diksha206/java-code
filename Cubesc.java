170b370b35210680fa56ea319fb3ff4d0d106d34
Diksha206 <dikshabhalke10@gmail.com>
2025-07-16 06:38:00 +0530

    Add files via upload

---
+++ Cubesc.java
@@ -0,0 +1,18 @@
+package imp;
+import java.util.Scanner;
+public class Cubesc {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("value is a is:");
+		int a= sc.nextInt();
+		System.out.println("the square is:"+(a*2));
+		System.out.println("the cube is:"+(a*3));
+		sc.close();
+		
+		
+		
+	}
+	
+
+}
