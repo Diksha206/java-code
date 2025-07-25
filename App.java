6e494f973b4dfeb2fadc11f27d06d6565ddc9018
Diksha206 <dikshabhalke10@gmail.com>
2025-07-25 20:11:14 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+package jjava;
+
+public class App {
+	static 
+	{
+		System.out.println("App Starting....");
+	}
+		public static void main(String[] args)
+		{
+		System.out.println("App runing...");
+	}
+}
