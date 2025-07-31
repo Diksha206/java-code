91bf441806cdd0ec4969e00242fabdfe135a758c
Diksha206 <dikshabhalke10@gmail.com>
2025-07-31 12:08:15 +0530

    Add files via upload

---
+++ Ifelsevote.java
@@ -0,0 +1,20 @@
+package Demo;
+import java.util.Scanner;
+public class Ifelsevote {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+System.out.println("enter the age:");
+int age=sc.nextInt();
+    if(age>=18)
+    {
+    	System.out.println("you are eligible to vote:");
+    }
+    else
+    {
+    	System.out.println("you are not eligible to vote:");
+    }
+
+	sc.close();
+	}
+	}
