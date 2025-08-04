72853b204f697aad6940766dd3b6c4f1a28da432
Diksha206 <dikshabhalke10@gmail.com>
2025-08-04 16:24:36 +0530

    Add files via upload

---
+++ IfelsePrime.java
@@ -0,0 +1,22 @@
+package condition;
+
+import java.util.Scanner;
+
+public class IfelsePrime {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int num=sc.nextInt();
+			if(num%2!=0)
+			{
+			 System.out.println("the num is prime:");
+			}
+			 else
+			{
+				System.out.println("the number is not prime:");
+		sc.close();
+
+}
+}
+}
