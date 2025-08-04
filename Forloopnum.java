59d8ee8ada14759ff6f314412e4de385546bda9f
Diksha206 <dikshabhalke10@gmail.com>
2025-08-04 16:43:42 +0530

    Add files via upload

---
+++ Forloopnum.java
@@ -0,0 +1,18 @@
+//Print Numbers from 1 to 100
+//Write a program that prints numbers from 1 to 100 using a for loop.
+
+
+package condition;
+
+public class Forloopnum {
+	public static void main(String[] args)
+	{
+		int i;
+		for( i = 1;i<=100;i++){
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
