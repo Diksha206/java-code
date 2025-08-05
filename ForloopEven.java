55124b87fdcd192e80730aaabfd28f436104c0c6
Diksha206 <dikshabhalke10@gmail.com>
2025-08-05 12:01:19 +0530

    Add files via upload

---
+++ ForloopEven.java
@@ -0,0 +1,16 @@
+//Print Even Numbers from 2 to 100
+//Print all even numbers from 2 to 100 using only a for loop.
+
+package condition;
+
+public class ForloopEven {
+	public static void main(String[] args)
+	{
+		int i;
+		for(i=2;i<=100;i+=2)
+		{
+			System.out.println(i);
+		}
+	
+}
+}
