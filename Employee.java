99afafe088b62caf354172c8c056fe085e628516
Diksha206 <dikshabhalke10@gmail.com>
2025-09-14 16:17:10 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,40 +1,34 @@
-package jjava;
-import java.util.Scanner;
+package com.inheritance;
+
 public class Employee {
+    private int empId;
+    private String empName;
+    private String empDesignation;
+    private double empSal;
 
+    public Employee(int empId, String empName, String empDesignation, double empSal) {
+        this.empId = empId;
+        this.empName = empName;
+        this.empDesignation = empDesignation;
+        this.empSal = empSal;
+    }
 
-	static String companyName;      //static variable
-	int Id;     //instance variable
-	String Name;
-	float Salary;
-	
-	static {
-		companyName="Tcs";
-	}
-	
-	Employee(int Id,String Name,float Salary)//contructor to initialize name,id,salary
-	{
-		this.Id = Id;
-		this.Name = Name;
-		this.Salary = Salary;
-	}
-	void Display()//display method
-	{
-		System.out.println("Employee Id :"+Id);
-		System.out.println("Employee Name :"+Name);
-		System.out.println("Employee Salary :"+Salary);
-		System.out.println("Employee Company Name :"+companyName);
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);      //use scanner to input data
-		System.out.printf("Enter Employee Id :-");
-		int id = sc.nextInt();
-		System.out.printf("Enter Employee Name :-");
-		String name = sc.next();
-		System.out.printf("Enter Employee salary43:-");
-		float salary = sc.nextFloat();
-		Employee e1 = new Employee(id,name,salary);
-		e1.Display();
-		sc.close();
-	}
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", empDesignation='" + empDesignation + '\'' +
+                ", empSal=" + empSal +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(101, "John Doe", "Manager", 50000.0);
+        Employee employee2 = new Employee(102, "Jane Smith", "Developer", 45000.0);
+
+        System.out.println(employee1);
+        System.out.println(employee2);
+    }
 }
+
