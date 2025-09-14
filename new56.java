ed954d477ef9a7253818f7ac9b1a092a0ade1e3d
Diksha206 <dikshabhalke10@gmail.com>
2025-09-14 16:33:47 +0530

    Add files via upload

---
+++ new56.java
@@ -0,0 +1,18 @@
+package com.inheritance;
+
+public class new56 {
+
+	  public static void main(String[] args) {
+
+	    try {
+
+	      // code that generate exception
+	      int divideByZero = 5 / 0;
+	      System.out.println("Rest of code in try block");
+	    }
+	    
+	    catch (ArithmeticException e) {
+	      System.out.println("ArithmeticException => " + e.getMessage());
+	    }
+	  }
+	}
