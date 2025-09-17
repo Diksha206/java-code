9a6e9ec5c27457d2ed99143aca2b453244b38d91
Diksha206 <dikshabhalke10@gmail.com>
2025-09-17 21:13:39 +0530

    Add files via upload

---
+++ Testawt2.java
@@ -0,0 +1,19 @@
+package Demo;
+
+
+import java.awt.*;
+public class Testawt2
+{
+  Testawt2()
+  {
+    Frame fm=new Frame();    //Creating a frame
+    Label lb = new Label("welcome to java graphics");   //Creating a label
+    fm.add(lb);                  //adding label to the frame
+    fm.setSize(300, 300);   //setting frame size.
+    fm.setVisible(true);     //set frame visibilty true
+  }
+  public static void main(String args[])
+  {
+    Testawt ta = new Testawt();
+  }
+}
