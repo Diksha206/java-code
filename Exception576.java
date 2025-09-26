58f48363431e62fc4d26c4e438bc5a218c70ec3c
Diksha206 <dikshabhalke10@gmail.com>
2025-09-26 18:22:21 +0530

    Add files via upload

---
+++ Exception576.java
@@ -0,0 +1,20 @@
+package Demo3;
+
+public class Exception576 {
+	public static void main(String[] args)
+	{
+	try {
+		int num1=123;
+		int num=Integer.parseInt("abc");
+		}
+	catch(Exception e)
+	{
+		
+	}
+	finally
+	{
+		System.out.println("Hii");
+	
+}
+}
+}
